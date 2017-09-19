99765bec56ba6f44a89522e789dbe8979d34ef99
xxb0216 <xiaoxiaobin@cqsf.com>
2017-09-19 17:21:30 +0800

    增加竣工移交记录列表页
    已测试数据

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var bodyParser=require("body-parser")
 var port = process.env.PORT || 3000
 var app = express()
 
-mongoose.connect('mongodb://localhost/imooc')
+//mongoose.connect('mongodb://localhost/imooc')
 
 app.set('views', './views/pages')
 app.set('view engine', 'pug')
@@ -38,7 +38,80 @@ movie:movie
 app.get('/admin/list',function(req,res){
 res.render('list',{
 title:'demo1 列表页',
-movies:movie
+movies:[{
+_id:1,
+director:'javan',
+country:'china',
+title:'钢铁侠',
+year:2014,
+poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+language:'chinese',
+flash:'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
+summary:'中国制造中国制造中国制造中国制造中国制造中国制造中国制造中国制造中国制造'
+},{
+_id:2,
+director:'javan',
+country:'china',
+title:'钢铁侠',
+year:2014,
+poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+language:'chinese',
+flash:'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
+summary:'中国制造中国制造中国制造中国制造中国制造中国制造中国制造中国制造中国制造'
+}]
+});
+});
+
+//jgyjjllist page
+app.get('/admin/jgyjjllist',function(req,res){
+res.render('jgyjjllist',{
+title:'竣工移交记录显示页',
+jgyjjls:[{
+_id:1,
+chexing:'CRH2A',
+liehao:'CRH2001',
+xiucheng:'五级修',
+peishuju:'兰州局',
+shangbuDate:'2017.9.10',
+shangbuPerson:'刘承波',
+xiabuDate:'2017.9.10',
+xiabuPerson:'肖晓斌',
+jgyjjldate:'2017.9.10',
+jgyjjlSN:'091JLJ103201709005',
+jgyjjlPerson:'肖晓斌',
+beizhu:'肖晓斌',
+bianzu:'9月19日编组'
+},{
+_id:1,
+chexing:'CRH2A',
+liehao:'CRH2001',
+xiucheng:'五级修',
+peishuju:'兰州局',
+shangbuDate:'2017.9.10',
+shangbuPerson:'刘承波',
+xiabuDate:'2017.9.10',
+xiabuPerson:'肖晓斌',
+jgyjjldate:'2017.9.10',
+jgyjjlSN:'091JLJ103201709005',
+jgyjjlPerson:'肖晓斌',
+beizhu:'肖晓斌',
+bianzu:'9月19日编组'
+},{
+_id:1,
+chexing:'CRH2A',
+liehao:'CRH2001',
+xiucheng:'五级修',
+peishuju:'兰州局',
+shangbuDate:'2017.9.10',
+shangbuPerson:'刘承波',
+xiabuDate:'2017.9.10',
+xiabuPerson:'肖晓斌',
+jgyjjldate:'2017.9.10',
+jgyjjlSN:'091JLJ103201709005',
+jgyjjlPerson:'肖晓斌',
+beizhu:'肖晓斌',
+bianzu:'9月19日编组'
+}]
 });
 });
 
