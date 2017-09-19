30788c11228e31537ad284633cf7541164f5501c
xxb0216 <xiaoxiaobin@cqsf.com>
2017-09-19 22:55:24 +0800

    修改错误

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser=require("body-parser")
 var port = process.env.PORT || 3000
 var app = express()
 
-mongoose.connect('mongodb://localhost/imooc')
+//mongoose.connect('mongodb://localhost/imooc')
 
 app.set('views', './views/pages')
 app.set('view engine', 'pug')
@@ -43,6 +43,7 @@ app.get('/movie/:id',function(req,res){
 			movie: movie
 		})
 	})
+
 	res.render('index',{
 		title:'demo1 首页1',
 		movies:movie
@@ -51,10 +52,10 @@ app.get('/movie/:id',function(req,res){
 
 //detail page
 app.get('/movie/:id',function(req,res){
-res.render('detail',{
-title:'demo1 详情页',
-movie:movie
-})
+	res.render('detail',{
+		title:'demo1 详情页',
+		movie:movie
+	})
 })
 
 //list page
@@ -68,11 +69,13 @@ app.get('/admin/list',function(req,res){
 			movies:movies
 		})
 	})
-res.render('list',{
-title:'demo1 列表页',
-movies: movies
-});
-});
+	res.render('list',{
+		title:'demo1 列表页',
+		movies: movies
+	})
+})
+
+
 //admin update movie
 app.get('/admin/update/:id', function (req, res) {
 	var id = req.params.id
@@ -135,70 +138,70 @@ app.post('/admin/movie/new',function(req, req){
 
 //jgyjjllist page
 app.get('/admin/jgyjjllist',function(req,res){
-res.render('jgyjjllist',{
-title:'竣工移交记录显示页',
-jgyjjls:[{
-_id:1,
-chexing:'CRH2A',
-liehao:'CRH2001',
-xiucheng:'五级修',
-peishuju:'兰州局',
-shangbuDate:'2017.9.10',
-shangbuPerson:'刘承波',
-xiabuDate:'2017.9.10',
-xiabuPerson:'肖晓斌',
-jgyjjldate:'2017.9.10',
-jgyjjlSN:'091JLJ103201709005',
-jgyjjlPerson:'肖晓斌',
-beizhu:'肖晓斌',
-bianzu:'9月19日编组'
-},{
-_id:1,
-chexing:'CRH2A',
-liehao:'CRH2001',
-xiucheng:'五级修',
-peishuju:'兰州局',
-shangbuDate:'2017.9.10',
-shangbuPerson:'刘承波',
-xiabuDate:'2017.9.10',
-xiabuPerson:'肖晓斌',
-jgyjjldate:'2017.9.10',
-jgyjjlSN:'091JLJ103201709005',
-jgyjjlPerson:'肖晓斌',
-beizhu:'肖晓斌',
-bianzu:'9月19日编组'
-},{
-_id:1,
-chexing:'CRH2A',
-liehao:'CRH2001',
-xiucheng:'五级修',
-peishuju:'兰州局',
-shangbuDate:'2017.9.10',
-shangbuPerson:'刘承波',
-xiabuDate:'2017.9.10',
-xiabuPerson:'肖晓斌',
-jgyjjldate:'2017.9.10',
-jgyjjlSN:'091JLJ103201709005',
-jgyjjlPerson:'肖晓斌',
-beizhu:'肖晓斌',
-bianzu:'9月19日编组'
-}]
-});
-});
+	res.render('jgyjjllist',{
+		title:'竣工移交记录显示页',
+			jgyjjls:[{
+				_id:1,
+				chexing:'CRH2A',
+				liehao:'CRH2001',
+				xiucheng:'五级修',
+				peishuju:'兰州局',
+				shangbuDate:'2017.9.10',
+				shangbuPerson:'刘承波',
+				xiabuDate:'2017.9.10',
+				xiabuPerson:'肖晓斌',
+				jgyjjldate:'2017.9.10',
+				jgyjjlSN:'091JLJ103201709005',
+				jgyjjlPerson:'肖晓斌',
+				beizhu:'肖晓斌',
+				bianzu:'9月19日编组'
+			},{
+				_id:1,
+				chexing:'CRH2A',
+				liehao:'CRH2001',
+				xiucheng:'五级修',
+				peishuju:'兰州局',
+				shangbuDate:'2017.9.10',
+				shangbuPerson:'刘承波',
+				xiabuDate:'2017.9.10',
+				xiabuPerson:'肖晓斌',
+				jgyjjldate:'2017.9.10',
+				jgyjjlSN:'091JLJ103201709005',
+				jgyjjlPerson:'肖晓斌',
+				beizhu:'肖晓斌',
+				bianzu:'9月19日编组'
+			},{
+				_id:1,
+				chexing:'CRH2A',
+				liehao:'CRH2001',
+				xiucheng:'五级修',
+				peishuju:'兰州局',
+				shangbuDate:'2017.9.10',
+				shangbuPerson:'刘承波',
+				xiabuDate:'2017.9.10',
+				xiabuPerson:'肖晓斌',
+				jgyjjldate:'2017.9.10',
+				jgyjjlSN:'091JLJ103201709005',
+				jgyjjlPerson:'肖晓斌',
+				beizhu:'肖晓斌',
+				bianzu:'9月19日编组'
+		}]
+	})
+})
 
 //admin page
 app.get('/admin/movie',function(req,res){
-res.render('admin',{
-title:'demo1 后台录入页',
-movie:{
-director:'',
-country:'',
-title:'',
-year:'',
-poster:'',
-language:'',
-flash:'',
-summary:''
-}
-});
-});
+	res.render('admin',{
+		title:'demo1 后台录入页',
+		movie:{
+			director:'',
+			country:'',
+			title:'',
+			year:'',
+			poster:'',
+			language:'',
+			flash:'',
+			summary:''
+		}
+	})
+})
