aa8b7131a42be62be16eb746958843be04be2b7d
xxb0216 <xiaoxiaobin@cqsf.com>
2017-09-21 00:16:57 +0800

    User

---
+++ schemas/user.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose')
+
+var UserSchema = new mongoose.Schema({
+	name:String,
+	job:String,
+	position:String,
+	technicalTitle:String,
+	attendance:String,
+	flash:String,
+	year:Number,
+	meta: {
+		creatAt:{
+			type:Date,
+			default: Date.now()
+		},
+		updateAt:{
+			type:Date,
+			default: Date.now()
+		}
+	}
+})
+
+MovieSchema.pre('save',function(next){
+	if (this.isNew){
+		this.meta.creatAt = this.meta.updateAt =Date.now()
+	}
+	else{
+		this.meta.updateAt =Date.now()
+	}
+
+	next()
+})
+
+MovieSchema.statics = {
+	fetch: function (cb) {
+		// body...
+		return this
+			.find({})
+			.sort('meta.updateAt')
+			.exec(cb)
+	},
+	findById: function (id,cb) {
+		// body...
+		return this
+			.findOne({_id:id})
+			.exec(cb)
+	}	
+}
+
+module.exports =UserSchema
